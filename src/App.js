902d25a6414d6928476c26f01efa89acb30f48ae
sanitech <sanitech47@gmail.com>
2024-11-04 20:59:40 +0300

    router compleat

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Router, Routes } from "react-router-dom";
+import Home from "./page/Home";
+import Room from "./page/Room";
+import RoomDetail from "./page/RoomDetail";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/room" element={<Room />} />
+        <Route path="/room/:roomId" element={<RoomDetail />} />
+        <Route path="*" element={<h1>Not found</h1>} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
